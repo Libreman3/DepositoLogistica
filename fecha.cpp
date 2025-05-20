aeb3ce61d41516bc8d7ad7524008eb8d38f8bd27
santicabrerasemper <santicabrerasemper8@gmail.com>
2025-05-20 19:25:09 -0300

    Se agrega el objeto Fecha (Fecha.h y Fecha.cpp)

---
+++ fecha.cpp
@@ -0,0 +1,36 @@
+#include "Fecha.h"
+
+void Fecha::Cargar() {
+    do {
+        std::cout << "DIA (1-31): ";
+        std::cin  >> dia;
+        if (dia < 1 || dia > 31)
+            std::cout << "  Valor invalido.  Por favor reingresa dia.\n";
+    } while (dia < 1 || dia > 31);
+
+    do {
+        std::cout << "MES (1-12): ";
+        std::cin  >> mes;
+        if (mes < 1 || mes > 12)
+            std::cout << "  Valor invalido. Por favor reingresa mes.\n";
+    } while (mes < 1 || mes > 12);
+
+    do {
+        std::cout << "ANIO (>=2025): ";
+        std::cin  >> anio;
+        if (anio < 2025)
+            std::cout << "  No se admiten anios anteriores a 2025. Por favor reingresa año.\n";
+    } while (anio < 2025);
+}
+
+void Fecha::Mostrar() const {
+    if (dia < 10)  std::cout << '0';
+    std::cout << dia << '/';
+    if (mes < 10)  std::cout << '0';
+    std::cout << mes << '/';
+    std::cout << anio << '\n';
+}
+
+
+
+
